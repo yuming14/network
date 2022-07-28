57cf88548164231280eb589867c6937d08196324
Ziming Gan <97362287+yuming14@users.noreply.github.com>
2022-07-27 23:07:09 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ import networkx as nx
 from pyvis.network import Network
 import numpy as np
 
+# Set header title
+st.title('Network Graph Visualization')
+
+# Set Nrow
+N_rows = st.slider("Nrows of edges to read in: ", min_value=1, max_value=1000000)
+
 # Read dataset (CSV)
 
-got_data = pd.read_csv('network_vec_small.csv', nrows = 10000)
+#got_data = pd.read_csv('network_vec.csv', sep="\s+", nrows = N_rows)
+got_data = pd.read_csv('network_vec_small.csv', sep=",", nrows = N_rows)
 got_dict = pd.read_csv('dict.csv', sep=",")
 got_dict.index = range(1,61010)
 title = got_dict['code']
@@ -43,15 +50,14 @@ code_list = title[idx].str.cat(desc[idx], sep=': ')
 code_list = code_list.tolist()
 code_list, idx = (list(t) for t in zip(*sorted(zip(code_list, idx))))
 
-
-# Set header title
-st.title('Network Graph Visualization')
-
-
 # Implement multiselect dropdown menu for option selection (returns a list)
 selected_codes = st.multiselect('Select code(s) to visualize', code_list)
 selected_idx_idx = [code_list.index(i) for i in selected_codes]
 selected_idx = [idx[i] for i in selected_idx_idx]
+st.write("Nselected ="+str(len(selected_idx)))
+
+target_fdr = st.slider("Target -log_10(Fdr)", min_value=0, max_value=50)
+st.write("target fdr =", 1/np.power(10, target_fdr))
 
 # Set info message on initial site load
 if len(selected_codes) == 0:
@@ -61,11 +67,14 @@ if len(selected_codes) == 0:
 else:
 
     # Initiate PyVis network object
-    code_net = Network(height='465px', bgcolor='white', font_color='white')
+    code_net = Network(height='800px', bgcolor='#222222', font_color='white')
 
     # Create networkx graph object from pandas dataframe
-    df_select = got_data.loc[got_data['row'].isin(selected_idx) | \
-                             got_data['col'].isin(selected_idx)]
+    tmp = (got_data['row'].isin(selected_idx) | \
+           got_data['col'].isin(selected_idx))
+    tmp = tmp & (got_data['fdr'] <= (1/np.power(10, target_fdr)))
+    df_select = got_data.loc[tmp]
+    del tmp
     df_select = df_select.reset_index(drop=True)
     if len(df_select) == 0:
         st.text('Incorrect')
@@ -91,7 +100,7 @@ else:
         code_net.add_edge(title[src]+': '+desc[src], 
                           title[dst]+': '+desc[dst], 
                           value = w*0.5, 
-                          color = {'color': 'whitesmoke', 'highlight': 'dimgrey'})
+                          color = {'color': 'dimgrey', 'highlight': 'whitesmoke'})
 
     neighbor_map = code_net.get_adj_list()
 
@@ -129,4 +138,4 @@ else:
     #HtmlFile = open(f'html_files/pyvis_graph.html', 'r', encoding='utf-8')
 
     # Load HTML file in HTML component for display on Streamlit page
-    components.html(HtmlFile.read(), height=435)
+    components.html(HtmlFile.read(), height=600, width=1000)
