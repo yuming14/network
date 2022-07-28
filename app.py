86a327b43b62f5e936abb48962dae5598279f6e3
Ziming Gan <97362287+yuming14@users.noreply.github.com>
2022-07-28 12:08:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,10 @@ else:
     # Create networkx graph object from pandas dataframe
     tmp = []
     for selected_idx_part in selected_idx:
-      tmp_part = [got_data['row'].tolist().index(selected_idx_part),
-                  got_data['col'].tolist().index(selected_idx_part)]
+      #tmp_part = [got_data['row'].tolist().index(selected_idx_part),
+      #            got_data['col'].tolist().index(selected_idx_part)]
+      tmp_part = [i for i,x in enumerate(got_data['row'].tolist()) if x == selected_idx_part ] + \
+                 [i for i,x in enumerate(got_data['col'].tolist()) if x == selected_idx_part ]
       tmp_part.sort()
       #tmp_part = tmp_part[0:(min(len(tmp_part),N_edge_select))]
       tmp = tmp + tmp_part
